fef2cfef2fe44bbf80f1c2ca045931a66f9a5a56
Jason Wu <wujason19@gmail.com>
2025-02-11 01:30:20 -0500

    committing this message

---
+++ QueueAccept.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Users\wujas\AppData\Local\Programs\Python\Python313\python.exe "C:\Users\wujas\Desktop\Learning Python\QueueAcceptService\test.py"
+exit
---
+++ QueueAccept.vbs
@@ -0,0 +1,2 @@
+Set objShell = CreateObject("WScript.Shell")
+objShell.Run """C:\Users\wujas\Desktop\Learning Python\QueueAcceptService\QueueAccept.bat""", 0, False
